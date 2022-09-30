bc1c0a5219192ddcd89a18c08c480e062340d5f2
Annabelle Perbet <annabelleperbet@yahoo.fr>
2022-09-30 18:27:56 +0200

    Création du composant AboutBanner et intégration de celui-ci dans la page about

---
+++ src/components/AboutBanner.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const AboutBanner = () => {
+  return (
+    <div className="about__banner">
+      <img
+        src="./aboutBanner.png"
+        alt="Paysage de montagnes"
+        className="about__banner__img"
+      />
+    </div>
+  );
+};
+
+export default AboutBanner;
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Header from "../components/Header";
+import AboutBanner from "../components/AboutBanner";
 
 const About = () => {
   return (
     <div>
       <Header />
+      <main>
+        <AboutBanner />
+      </main>
     </div>
   );
 };
